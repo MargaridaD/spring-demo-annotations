9cb3e5dacc2826ea6ae1bf78a6eedc380d2c11bf
MargaridaD <manunciacao@gmail.com>
2022-05-10 07:45:32 +0100

    até video 85
    Até configuração com Java Code

--- src/com/luv2code/springdemo/JavaConfigDemoApp.java
+++ src/com/luv2code/springdemo/JavaConfigDemoApp.java
@@ -1,14 +1,14 @@
 package com.luv2code.springdemo;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class AnnotationDemoApp {
+public class JavaConfigDemoApp {
 
 	public static void main(String[] args) {
 
-		// read spring config file
-		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-		
+		// read spring config java class - Colocar a class com as configuracoes (SportConfig)
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SportConfig.class);
 		
 		//get the bean from spring container - Default bean Id = Nome da Classe, mas a começar com letra minuscula
 		Coach theCoach = context.getBean("tennisCoach", Coach.class);
@@ -19,8 +19,7 @@ public class AnnotationDemoApp {
 		
 		// call method to get the daily fortune
 		System.out.println(theCoach.getDailyFortune());
-		
-		
+			
 		//close the context
 		context.close();
 		
